72b0a6cf951c72a95f13afb6cf985871ee53e024
Tom R <tom.ruette@gmail.com>
2017-05-21 13:25:16 +0200

    some smaller issues fixed

--- code.r
+++ code.r
@@ -38,15 +38,42 @@ spdf <- getCountries(c("NLD", "BEL"), level=1)
 citetaal = read.csv(url("https://raw.githubusercontent.com/ruettet/citetaal/master/cite.csv"), sep=";")
 
 # fix the lat lon as string
-citetaal$lat = as.numeric(as.vector(citetaal$lat))
-citetaal$lon = as.numeric(as.vector(citetaal$lon))
+citetaal$lat =  as.numeric(gsub(",", ".", as.vector(citetaal$lat)))
+citetaal$lon =  as.numeric(gsub(",", ".", as.vector(citetaal$lon)))
+
+############################################################################################
+# Niet variationele kaarten van palatalisatie in het algemeen
+############################################################################################
 
 # fetch the palatalization observations
-citetaal.pal = citetaal[ which(citetaal$shibbolet == "pal"), ]
+citetaal.pal = citetaal[ which(citetaal$tweet_observation_variable == "palatalisation" & 
+			       citetaal$tweet_observation_variant == "cite"), ]
 
 # initialize the map
 plot(spdf, border="darkgrey", main="Observations of palatalization are mainly in Flanders")
 map("rivers", add=TRUE, col="cornflowerblue")
 
 # plot the palatalization observations on map
-points(citetaal.pal$lon, citetaal.pal$lat, pch=20, col="red"
+points(citetaal.pal$lon, citetaal.pal$lat, pch=20, col="red")
+
+############################################################################################
+# Niet variationele kaarten van palatalisatie "shtijl"
+############################################################################################
+
+# fetch the stijl-palatalization observations
+citetaal.pal.stijl = citetaal[ which(citetaal$tweet_observation_variable == "palatalisation" && citetaal$tweet_observation_), ]
+
+# initialize the map
+plot(spdf, border="darkgrey", main="Observations of palatalization are mainly in Flanders")
+map("rivers", add=TRUE, col="cornflowerblue")
+
+# plot the palatalization observations on map
+points(citetaal.pal$lon, citetaal.pal$lat, pch=20, col="red")
+
+############################################################################################
+# Niet variationele kaarten van palatalisatie "shtijl", jaar per jaar
+############################################################################################
+
+############################################################################################
+# Niet variationele kaarten van intensificatie met "vies"
+############################################################################################
