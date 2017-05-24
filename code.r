a1b49d31af8825fb3f1c895d696e4317210fe170
Tom R <tom.ruette@gmail.com>
2017-05-24 22:31:58 +0200

    synchronic maps for intens. and stylisation

--- code.r
+++ code.r
@@ -46,18 +46,18 @@ pivotAndPlot <- function(selected_data) {
 	points(summarised$longitude, summarised$latitude, pch=20, cex=(summarised$count / max(summarised$count)) + 1, col="red")
 }
 
-pivotAndProportionAndPlot <- function(selected_data) {
+pivotAndProportionFactorAndPlot <- function(selected_data, grouper, a, b) {
 	# build a lat/lon source
 	latLon = as.data.frame(summarise(group_by(selected_data, loc), latitude=mean(lat), longitude=mean(lon)))
 	
 	# count the cite and not cite observations
-	grouping = group_by(selected_data, loc, tweet_observation_variant)
+	grouping = group_by_(selected_data, 'loc', grouper)
 	summarised = summarise(grouping, count=n())
 	
 	# turn into wide format as to be able to take proportion of cite observations
-	summarised.wide = spread(summarised, tweet_observation_variant, count)
+	summarised.wide = spread_(summarised, grouper, 'count')
 	summarised.wide[is.na(summarised.wide)] <- 0
-	ratios = summarised.wide['cite'] / (summarised.wide['cite'] + summarised.wide['not cite'])
+	ratios = summarised.wide[a] / (summarised.wide[a] + summarised.wide[b])
 	
 	# add to lat/lon source
 	latLon$ratios = ratios
@@ -67,7 +67,7 @@ pivotAndProportionAndPlot <- function(selected_data) {
 	map("rivers", add=TRUE, col="cornflowerblue")
 
 	# plot the palatalization observations on map
-	points(latLon$longitude, latLon$latitude, pch=20, cex=1, col=rgb(latLon$ratios$cite, 0, 0))
+	points(latLon$longitude, latLon$latitude, pch=20, cex=1, col=rgb(latLon$ratios[[a]], 0, 0))
 }
 
 # load the maps
@@ -78,6 +78,7 @@ google_docs_url = "https://docs.google.com/spreadsheets/d/1JtJ6SSaQxI6Ayx2mAyM3r
 citetaal = read.csv(url(google_docs_url), sep=",")
 
 # fix the lat lon as string
+# TODO: fix this in the datasource
 citetaal$lat =  as.numeric(gsub(",", ".", as.vector(citetaal$lat)))
 citetaal$lon =  as.numeric(gsub(",", ".", as.vector(citetaal$lon)))
 
@@ -89,14 +90,12 @@ par(mfrow=c(2,3))
 for (year in c(2012, 2013, 2014, 2015, 2016)) 
 {
 	# fetch the palatalization observations
-	citetaal.pal = citetaal[ which(citetaal$tweet_observation_variable == "palatalisation" & 
-				       citetaal$tweet_observation_variant == "cite" &
-				       citetaal$loc != "" & 
-				       citetaal$year == year), ]
+	citetaal.pal = citetaal[which(citetaal$tweet_observation_variable == "palatalisation" & 
+				      citetaal$tweet_observation_variant == "cite" &
+				      citetaal$loc != "" & 
+				      citetaal$year == year), ]
 	pivotAndPlot(citetaal.pal)
 }
-
-
 
 ############################################################################################
 # KAART 3. Niet variationele kaart van palatalisatie "shtijl", jaar per jaar
@@ -106,34 +105,70 @@ par(mfrow=c(2,3))
 for (year in c(2012, 2013, 2014, 2015, 2016)) 
 {
 	# fetch the palatalization observations
-	citetaal.stijlpal = citetaal[ which(citetaal$tweet_observation_variable == "palatalisation" & 
-				            citetaal$tweet_observation_variant == "cite" &
-				            citetaal$shibboleth == "stijl" &
-				            citetaal$loc != "" & 
-				            citetaal$year == year), ]
+	citetaal.stijlpal = citetaal[which(citetaal$tweet_observation_variable == "palatalisation" & 
+				           citetaal$tweet_observation_variant == "cite" &
+				           citetaal$shibboleth == "stijl" &
+				           citetaal$loc != "" & 
+				           citetaal$year == year), ]
 	pivotAndPlot(citetaal.stijlpal)
 }
-
 
 ############################################################################################
 # KAART 4. Variationele kaart (cite/not cite) van lemma "stijl" in "style" betekenis
 ############################################################################################
 
 # fetch the palatalization observations
-citetaal.stijl.style = citetaal[ which(citetaal$shibboleth == "stijl" & 
-			               citetaal$meaning == "style" &
-			               citetaal$loc != ""), ]
+citetaal.stijl.style = citetaal[which(citetaal$shibboleth == "stijl" & 
+			              citetaal$meaning == "style" &
+			              citetaal$loc != ""), ]
 par(mfrow=c(1,1))
 pivotAndProportionAndPlot(citetaal.stijl.style)
-
 
 ############################################################################################
 # KAART 5. Variationele kaart (cite/not cite) van lemma "stijl" in "cool" betekenis
 ############################################################################################
 
 # fetch the palatalization observations
-citetaal.stijl.cool = citetaal[ which(citetaal$shibboleth == "stijl" & 
-			       citetaal$meaning == "cool" &
-			       citetaal$loc != ""), ]
+citetaal.stijl.cool = citetaal[which(citetaal$shibboleth == "stijl" & 
+			             citetaal$meaning == "cool" &
+			             citetaal$loc != ""), ]
 par(mfrow=c(1,1))
-pivotAndProportionAndPlot(citetaal.stijl.cool)
+pivotAndProportionFactorAndPlot(citetaal.stijl.cool, 'tweet_observation_variant', 'cite', 'not cite')
+
+############################################################################################
+# KAART 7. Variationele kaart (hashtag/no hashtag) van "stijl" in "cool" betekenis cite vorm
+############################################################################################
+
+citetaal.stijl.cool.cite = citetaal[which(citetaal$shibboleth == "stijl" &
+					  citetaal$meaning == "cool" &
+					  citetaal$loc != "" &
+					  citetaal$tweet_observation_variant == "cite"),]
+par(mfrow=c(1,1))
+pivotAndProportionFactorAndPlot(citetaal.stijl.cool.cite, 'stylisation', 'hashtag', 'no hashtag')
+
+
+############################################################################################
+# KAART 8. Variationele kaart (hashtag/no hashtag) van "stijl" in "cool" betekenis niet cite
+############################################################################################
+
+citetaal.stijl.cool.notcite = citetaal[which(citetaal$shibboleth == "stijl" &
+			                     citetaal$meaning == "cool" &
+					     citetaal$loc != "" &
+					     citetaal$tweet_observation_variant == "not cite"),]
+par(mfrow=c(1,1))
+pivotAndProportionFactorAndPlot(citetaal.stijl.cool.notcite, 'stylisation', 'hashtag', 'no hashtag')
+
+############################################################################################
+# KAART 9. Variationele kaart intensification (yes/no) van "stijl" in "cool" betekenis cite
+############################################################################################
+
+par(mfrow=c(1,1))
+pivotAndProportionFactorAndPlot(citetaal.stijl.cool.cite, 'intensification', 'yes', 'no')
+
+############################################################################################
+# KAART 10. Variationele kaart intensification (yes/no) van "stijl" in "cool" betekenis niet cite
+############################################################################################
+
+par(mfrow=c(1,1))
+pivotAndProportionFactorAndPlot(citetaal.stijl.cool.notcite, 'intensification', 'yes', 'no')
+
